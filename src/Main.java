2b72d5d6dea955a89fa609f0ca1b67c782176581
Jesdwarf/mads <s170185@student.dtu.dk>
2020-05-05 10:49:26 +0200

    user Authorizesation måvedule implementet.

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,22 @@
 import controller.DigitalOceanController;
 import model.Droplet;
 
+import java.net.MalformedURLException;
+import java.rmi.NotBoundException;
+import java.rmi.RemoteException;
+
 public class Main {
     public static void main(String[] args) {
         DigitalOceanController controller = new DigitalOceanController();
+        try {
+            controller.authorize("s170185", "Bamsefar");
+        } catch (RemoteException e) {
+            e.printStackTrace();
+        } catch (NotBoundException e) {
+            e.printStackTrace();
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        }
 
         System.out.println("Droplets:");
         for (Droplet d : controller.getDroplets()) {
--- src/controller/DigitalOceanController.java
+++ src/controller/DigitalOceanController.java
@@ -1,16 +1,47 @@
 package controller;
 
+import brugerautorisation.data.Bruger;
+import brugerautorisation.transport.rmi.Brugeradmin;
 import com.google.gson.Gson;
 import connection.DigitalOceanConnection;
 import model.Droplet;
 import model.Droplets;
 
+import java.net.MalformedURLException;
+import java.rmi.Naming;
+import java.rmi.NotBoundException;
+import java.rmi.RemoteException;
+
 public class DigitalOceanController {
     private DigitalOceanConnection conn = DigitalOceanConnection.getInstance();
 
+    private boolean isAuthorized = false;
+
 
     public Droplet[] getDroplets() {
-        Droplets droplets = new Gson().fromJson(conn.getUnirest().get("digitalocean/droplets").asJson().getBody().toString(), Droplets.class);
-        return droplets.droplets;
+        if (isAuthorized) {
+            Droplets droplets = new Gson().fromJson(conn.getUnirest().get("digitalocean/droplets").asJson().getBody().toString(), Droplets.class);
+            return droplets.droplets;
+        }
+        return null;
+    }
+
+    public void authorize(String username, String password) throws RemoteException, NotBoundException, MalformedURLException {
+        Brugeradmin ba = (Brugeradmin) Naming.lookup("rmi://javabog.dk/brugeradmin");
+
+
+        try {
+            Bruger user = ba.hentBruger(username, password);
+
+            isAuthorized = true;
+
+
+            System.out.println("user authorized!");
+
+
+        } catch (Exception e) {
+            System.out.println("user not authorized");
+        }
+
     }
 }
