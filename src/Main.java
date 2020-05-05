35ae1b485b508abae53dad32e168942e708c752b
Jesdwarf/mads <s170185@student.dtu.dk>
2020-05-05 10:55:13 +0200

    console program is MVP

--- src/Main.java
+++ src/Main.java
@@ -4,19 +4,25 @@ import model.Droplet;
 import java.net.MalformedURLException;
 import java.rmi.NotBoundException;
 import java.rmi.RemoteException;
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("username: ");
+        String username = input.nextLine();
+        System.out.println("password: ");
+        String password = input.nextLine();
+
         DigitalOceanController controller = new DigitalOceanController();
+
         try {
-            controller.authorize("s170185", "Bamsefar");
-        } catch (RemoteException e) {
-            e.printStackTrace();
-        } catch (NotBoundException e) {
-            e.printStackTrace();
-        } catch (MalformedURLException e) {
+            controller.authorize(username, password);
+        } catch (Exception e) {
             e.printStackTrace();
         }
+
 
         System.out.println("Droplets:");
         for (Droplet d : controller.getDroplets()) {
